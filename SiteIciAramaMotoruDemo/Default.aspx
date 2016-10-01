cbe7fe8d322f56540364b7af043d31a3708dbcc5
ismail fedakar <fedakaraydin09@hotmail.com>
2016-10-01 17:39:32 +0300

    SiteIciAramaMotoruDemo
    SiteIciAramaMotoruDemo

---
+++ SiteIciAramaMotoruDemo/Default.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="AramaDemo1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Site İçi Arama</title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 30px;
+        }
+        .auto-style3 {
+            height: 30px;
+            width: 73px;
+        }
+        .auto-style4 {}
+    </style>
+</head>
+
+<body style="margin:0px; padding:0px;">
+            <div style="margin:0px; padding:0px; width:100%">
+    <form id="form1" runat="server" style="margin:0px; padding:0px;">
+        <table width="100%" style="margin-top:0px;">
+            <tr>
+                <td class="auto-style3" align="left" style="border-collapse: collapse; line-height: 25px; vertical-align: middle; text-align: left">
+        <asp:Button ID="btnAra" runat="server" OnClick="btnAra_Click" Text="Ara" Font-Bold="False" Font-Size="14px" Height="30px" Width="60px" />
+                </td>
+                <td class="auto-style1">
+        <asp:TextBox ID="txtAra" runat="server" Height="22px" style="margin-left: 0px" Width="165px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4" colspan="2">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="Data Source=.;Initial Catalog=HaberPortali;Integrated Security=True" ProviderName="System.Data.SqlClient" SelectCommand="SELECT * FROM [Haberler]"></asp:SqlDataSource>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" style="height: 22px">
+                    <asp:GridView ID="GridView1" runat="server" Width="100%">
+                    </asp:GridView>
+                    </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+        <asp:Label ID="lblBilgi" runat="server" ForeColor="#333333" Font-Bold="False" Font-Size="14px" Font-Names="Verdana"></asp:Label>
+
+                </td>
+            </tr>
+        </table>
+
+    </form>
+                </div>
+</body>
+</html>
---
+++ SiteIciAramaMotoruDemo/Default.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AramaDemo1 : System.Web.UI.Page
+{
+    Fonksiyon system = new Fonksiyon();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        //Query Stringden gelen ara ismli paramtreyi alıyoruz ve null değilse
+        if (Request.QueryString["ara"] != null)
+        {
+            //Gelen parametreyi string bir değişkene atıyoruz.
+            //Dolayısıyla queryStringden gelen ara isimli değişkenin değeride aranan isimli dğişkende depolanıyor ve
+            //Bu değişkeni HaberleriGetir Metoduna parametre olarak gönderdiğimizde metottaki sql sorgusunda aranan değerde bu oluyor
+            string aranan = Request.QueryString["ara"];
+
+            //aranan değeri HaberleriGetir Metoduna parametre olarak gönderiyoruz.
+            HaberleriGetir(aranan);
+
+        }
+
+    }
+    protected void btnAra_Click(object sender, EventArgs e)
+    {
+        //Arama yapacağımız textbox un eğerini temizle metoduna gönderip sqlInjection u engelliyoruz.
+        string aranan = txtAra.Text;
+
+        if (aranan.Length < 3)
+        {
+            lblBilgi.Text = "En Az 3 Karakter Girmelisiniz!";
+            GridView1.Visible = false;
+            //Eğer 3 karakterden az değer girilirse sonuç dönmeyeceği için önceden ekranda bir değer varsa Datagridi gizliyoruzki herhangi bir değer dönmediği belli olsun
+
+        }
+        else
+        {
+            Response.Redirect("Default.aspx?ara=" + aranan);
+
+        }
+        //Response.Redirect("AramaDemo1.aspx");
+
+    }
+
+    //Haberleri veritabanından çekmek için metot tanımlıyoruz.
+    void HaberleriGetir(string aranan)
+    {
+        //Bağlantı kodlarımız.
+        SqlConnection baglanti = system.baglan();
+        //Veritabanından aldığımız kayıtları DataAdapter e atıyoruz.
+        SqlDataAdapter dAdapter = new SqlDataAdapter("select Baslik,Ozet,Detay from Haberler Where Baslik LIKE '%" + aranan + "%' OR Ozet LIKE '%" + aranan + "%' OR Detay LIKE '" + aranan + "%' order by HaberId desc", baglanti);
+
+        //Datatable tanımlıyoruz. Dataadapter için.
+        DataTable dtHaberler = new DataTable();
+
+        //Datatable i Adaptere atıyoruz.
+        dAdapter.Fill(dtHaberler);
+
+        //Datatable deki satır sayısını alıyoruz. Eğer Kayıt varsa. Okunan kayıtları GridWiev a bağlıyoruz.
+        if (dtHaberler.Rows.Count > 0)
+        {
+            GridView1.DataSource = dtHaberler;
+            GridView1.DataBind();
+            GridView1.Visible = true;
+
+            //Ardından bulunan kayıt sayısını rakam ile label e yazdırıp kayıt bulundu onu bildiriyoruz.
+            lblBilgi.Text = dtHaberler.Rows.Count + " adet sonuç bulunmuştur";
+            GridView1.Visible = true;
+        }
+
+        else
+        {
+            //Eğer girilen kelimeyi karşılayacak herangi bir kayıt bulunamazsada aşağıdaki kodu çalıştırıyoruz.
+            lblBilgi.Text = aranan + " kelimesi için sonuç bulunamadı";
+            GridView1.Visible = false;
+        }
+    }
+
+}
